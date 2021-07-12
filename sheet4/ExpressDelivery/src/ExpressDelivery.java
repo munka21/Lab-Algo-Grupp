728b614f8d7535269bf5efab3a0f5ba27c0a1f85
rnaum <58358448+rnaum@users.noreply.github.com>
2021-07-12 12:22:16 +0200

    Add files via upload

--- sheet4/ExpressDelivery/src/ExpressDelivery.java
+++ sheet4/ExpressDelivery/src/ExpressDelivery.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class ExpressDelivery {
@@ -43,11 +44,13 @@ public class ExpressDelivery {
 		}
 		
 		droneNetwork.addOptimalEdges();
+		double[][] minDist=droneNetwork.floydWarschall();
 		String output="";
 		for(int q=0; q<numberOfDeliveries; q++) {
 			String[] line= bi.readLine().split(" ");
-			droneNetwork.dikstra(Integer.parseInt(line[0])-1,Integer.parseInt(line[1])-1); //starting at 1 instead of 0
-			output+=df.format(droneNetwork.getNodes().get(Integer.parseInt(line[1])-1).dikstraDistance)+" ";
+			output+=df.format(Math.round(minDist[Integer.parseInt(line[0])-1][Integer.parseInt(line[1])-1]*100d)/100d)+" ";
+			//droneNetwork.dikstra(Integer.parseInt(line[0])-1,Integer.parseInt(line[1])-1); //starting at 1 instead of 0
+			//output+=df.format(Math.round(droneNetwork.getNodes().get(Integer.parseInt(line[1])-1).getDikstraDistance(Integer.parseInt(line[0])-1)*100d)/100d)+" ";
 		}
 		//remove empty space at the end:
 		output = output.substring(0, output.length() - 1);
@@ -127,11 +130,13 @@ public class ExpressDelivery {
 		private boolean checkForBetterEdge(Node startNode, Node endNode, double time) {
 			for(Edge e: startNode.outgoingEdges) {
 				if(e.endNode.stationNumber==endNode.stationNumber) {
-					if(e.timeNeeded< time) {
+					if(e.timeNeeded<= time) {
 						return true;
-					}
+					} 
 				}
 			}
+			
+			
 			return false;
 		}
 
@@ -164,43 +169,81 @@ public class ExpressDelivery {
 		public ArrayList<Node> getNodes(){
 			return nodes;
 		}
-		
-		public void dikstra(int startnumber, int endnumber) {
-			//initialize Queue
-			PriorityQueue<Node> queue= new PriorityQueue<Node>();
-			for(Node n: nodes) {
-				if(n.getStationNumber()!=startnumber) {
-					n.resetDikstraDistance();
-					queue.add(n);
-				}else {
-					n.setDikstraDistance(0);
-					queue.add(n);
+
+		public double[][] floydWarschall() {
+			double[][] minimalDistance = new double[this.numberOfNodes][this.numberOfNodes];
+			for (Node v : this.nodes) {
+				int i = v.stationNumber;
+				for (Node w : this.nodes) {
+					int j = w.stationNumber;
+					minimalDistance[i][j] = Integer.MAX_VALUE;
 				}
 			}
-			while(!queue.isEmpty()) {
-				Node u=queue.poll();
-				//if needed I could add that all shortest paths are stored away here
-				if(u.getStationNumber()==endnumber) {
-					break;
-				}else {
-					for(Edge e: u.getOutgoingEdges()) {
-						if(e.usable) {
-							Node v= e.getEndNode();
-							if(queue.contains(v)) {
-								//update distance if nessesary:
-								if(u.getDikstraDistance()+e.timeNeeded<v.getDikstraDistance()) {
+
+			for (Node n : this.nodes) {
+				minimalDistance[n.stationNumber][n.stationNumber] = 0;
+				for (Edge e : n.outgoingEdges) {
+					if (e.usable) {
+						if (minimalDistance[n.stationNumber][e.endNode.stationNumber] > e.timeNeeded) {
+
+							minimalDistance[n.stationNumber][e.endNode.stationNumber] = e.timeNeeded;
+						}
+					}
+				}
+			}
+
+			for (Node u : this.nodes) {
+				int k = u.stationNumber;
+				for (Node v : this.nodes) {
+					int i = v.stationNumber;
+					for (Node w : this.nodes) {
+						int j = w.stationNumber;
+						if (minimalDistance[i][j] > minimalDistance[i][k] + minimalDistance[k][j]) {
+							minimalDistance[i][j] = minimalDistance[i][k] + minimalDistance[k][j];
+						}
+					}
+				}
+			}
+			return minimalDistance;
+		}
+		public void dikstra(int startnumber, int endnumber) {
+			// initialize Queue
+
+			Node endNode = this.nodes.get(endnumber);
+			if (!endNode.dikstraDistance.containsKey(startnumber)) {
+
+				PriorityQueue<Node> queue = new PriorityQueue<Node>();
+				for (Node n : nodes) {
+					n.setStartNodeNumber(startnumber);
+					if (n.getStationNumber() != startnumber) {
+						n.resetDikstraDistance(startnumber);
+						queue.add(n);
+					} else {
+						n.setDikstraDistance(0);
+						queue.add(n);
+					}
+				}
+				while (!queue.isEmpty()) {
+					Node u = queue.poll();
+
+					for (Edge e : u.getOutgoingEdges()) {
+						if (e.usable) {
+							Node v = e.getEndNode();
+							if (queue.contains(v)) {
+								// update distance if nessesary:
+								if (u.getDikstraDistance() + e.timeNeeded < v.getDikstraDistance()) {
 									queue.remove(v);
-									v.setDikstraDistance(u.getDikstraDistance()+e.timeNeeded);
-									
-									queue.add(v); //sadly no resorting exists
+									v.setDikstraDistance(u.getDikstraDistance() + e.timeNeeded);
+
+									queue.add(v); // sadly no resorting exists
 								}
 							}
 						}
-						
+
 					}
-					
+
 				}
-				
+
 			}
 		}
 
@@ -214,7 +257,9 @@ public class ExpressDelivery {
 		private ArrayList<Edge> outgoingEdges;
 		private int kmh;
 		private int maxDistance;
-		private double dikstraDistance;
+		private int startNodeNumber;
+		
+		private HashMap<Integer, Double> dikstraDistance;
 		private boolean nonUsableEdge;
 		
 		
@@ -224,14 +269,23 @@ public class ExpressDelivery {
 			this.kmh=kmh;
 			this.maxDistance=maxDistance;
 			this.nonUsableEdge=false;
+			this.dikstraDistance=new HashMap<Integer, Double>();
 		}
 
 		public ArrayList<Edge> getOutgoingEdges() {
 			return this.outgoingEdges;
 		}
 
-		public void setDikstraDistance(double i) {
-			this.dikstraDistance=i;
+		public void setStartNodeNumber(int startNodeNumber) {
+			this.startNodeNumber=startNodeNumber;
+		}
+		public void setDikstraDistance(int nodeNumber, double i) {
+			this.dikstraDistance.put(nodeNumber, i);
+			
+		}
+		
+		public void setDikstraDistance( double i) {
+			this.dikstraDistance.put(this.startNodeNumber, i);
 			
 		}
 
@@ -248,8 +302,10 @@ public class ExpressDelivery {
 			return kmh;
 		}
 		
-		public void resetDikstraDistance() {
-			this.dikstraDistance=Integer.MAX_VALUE;
+		public void resetDikstraDistance(int nodeNumber) {
+			if(!this.dikstraDistance.keySet().contains(nodeNumber)) {
+				this.dikstraDistance.put(nodeNumber,  (double)Integer.MAX_VALUE);
+			}
 		}
 		
 		public void setNonUsableEdgeTrue() {
@@ -258,11 +314,14 @@ public class ExpressDelivery {
 
 		@Override
 		public int compareTo(Node arg0) {
-			return  (int) Math.signum(this.dikstraDistance-arg0.getDikstraDistance());
+			return  (int) Math.signum(this.dikstraDistance.get(this.startNodeNumber)-arg0.getDikstraDistance(this.startNodeNumber));
 		}
 
+		private double getDikstraDistance(int startNode) {
+			return this.dikstraDistance.get(startNode);
+		}
 		private double getDikstraDistance() {
-			return this.dikstraDistance;
+			return this.dikstraDistance.get(this.startNodeNumber);
 		}
 
 		@Override
